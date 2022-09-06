98f52570c04e5bc57106451d6f757cf9ad40da26
caranicas <kendrick.caranicas@gmail.com>
2022-09-06 16:43:06 -0400

    display and update prompt

--- ui/frontend/build/src/components/creationPanel/index.tsx
+++ ui/frontend/build/src/components/creationPanel/index.tsx
@@ -4,13 +4,26 @@ import MakeButton from "./makeButton";
 import AdvancedSettings from './advancedSettings';
 import ImageModifiers from './imageModifiers';
 
+import { useImageCreate } from "../../store/imageCreateStore";
+
+
 export default function CreationPanel() {
+
+  
+  const promptText = useImageCreate((state) => state.imageOptions.prompt);
+  const setPrompt = useImageCreate((state) => state.setPrompt);
+
+  const handlePromptChange = (event) => {
+    console.log("handlePromptChange", event.target.value);
+    setPrompt(event.target.value);
+  };
+
   return (
     <div className="create-panel">
       <div className="basic-create">
         <div className="prompt">
           <p>Prompt </p>
-          <textarea></textarea>
+          <textarea value={promptText} onChange={handlePromptChange}></textarea>
         </div>
 
         <div className="seed-image">
